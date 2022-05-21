8f4e0068a1676edf9ea875f953cefb43a44f18bd
henriqfelix <henriquefelix1821@gmail.com>
2022-05-20 21:34:04 -0300

    implementar teclas de comando

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,25 @@
 window.onload = function () {
   let stage = document.querySelector("#stage");
   let ctx = stage.getContext("2d");
+  document.addEventListener("keydown", keyPush);
 
   setInterval(game, 60);
 
   const vel = 1;
+
   let vx = (vy = 0);
-  let px = (py = 10);
+  let px = 10;
+  let py = 15;
   let tp = 20;
   let qp = 20;
   let ax = (ay = 15);
+
   let trail = [];
-  trail = 5;
+  let tail = 5;
 
   function game() {
     px += vx;
-    py += py;
+    py += vy;
 
     if (px < 0) {
       px = qp - 1;
@@ -37,22 +41,44 @@ window.onload = function () {
     ctx.fillRect(ax * tp, ay * tp, tp, tp);
 
     ctx.fillStyle = "gray";
-    for (let i; i < trail.length; i++) {
+    for (let i = 0; i < trail.length; i++) {
       ctx.fillRect(trail[i].x * tp, trail[i].y * tp, tp, tp);
-      if (trail[i].x === px && trail[i].y === py) {
+      if (trail[i].x == px && trail[i].y == py) {
         vx = vy = 0;
+        tail = 5;
       }
     }
 
     trail.push({ x: px, y: py });
-    while (trail.length > trail) {
+    while (trail.length > tail) {
       trail.shift();
     }
 
-    if (ax === px && ay === py) {
-      trail++;
+    if (ax == px && ay == py) {
+      tail++;
       ax = Math.floor(Math.random() * qp);
       ay = Math.floor(Math.random() * qp);
+    }
+  }
+
+  function keyPush(e) {
+    switch (e.keyCode) {
+      case 37: //left
+        vx = -vel;
+        vy = 0;
+        break;
+      case 38: //up
+        vx = 0;
+        vy = -vel;
+        break;
+      case 39: //right
+        vx = vel;
+        vy = 0;
+        break;
+      case 40: //down
+        vx = 0;
+        vy = vel;
+        break;
     }
   }
 };
